825e16f7fceee90a8b4d2c58b59f4fe3de7dbaf5
kathosgifre <109618322+kathosgifre@users.noreply.github.com>
2022-09-22 00:19:13 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.information');
+ScrollReveal().reveal('.experts', { delay:600});
+ScrollReveal().reveal('.proyects', { delay:700});
+ScrollReveal().reveal('.social', { delay:800});
