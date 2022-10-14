11840378928c96a843523f5b6fd6c66f0d8b4bfa
kathosgifre <109618322+kathosgifre@users.noreply.github.com>
2022-10-13 23:32:02 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,3 @@ ScrollReveal().reveal('.experts', { delay:600});
 ScrollReveal().reveal('.proyects', { delay:700});
 ScrollReveal().reveal('.social', { delay:800});
 
-<script type="text/javascript">
-   document.oncontextmenu = function(){return false;}
-</script>
